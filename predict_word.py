83fc8f662074e467e928669deacc7f1728f8cd16
TAWSIF AHMED <sleeping4cat@outlook.com>
2024-04-19 16:40:54 +0600

    added imports

--- predict_word.py
+++ predict_word.py
@@ -1,9 +1,15 @@
-def predict_word(prompt: str, model, tokenizer, top_k: int = 2) -> list[str]:
+from happytransformer import HappyWordPrediction
+
+happy_wp = HappyWordPrediction()
+
+def predict_word(prompt: str, model, top_k: int = 2) -> list[str]:
     # Add [MASK] to the prompt
     text = prompt + " [MASK]"
 
+    # Predict the masked word
     result = model.predict_mask(text, top_k=top_k)
 
+    # Extract token values from the result
     predictions = [prediction.token for prediction in result]
 
     return predictions
